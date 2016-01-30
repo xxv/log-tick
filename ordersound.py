cf8109ba52bf98ff6b00bc0b00fa926038c539e3
Steve Pomeroy <stevep@thelevelup.com>
2016-01-30 00:49:39 -0500

    Cleans up code; adds filter for 200 status code

--- ordersound.py
+++ ordersound.py
@@ -9,30 +9,35 @@ import time
 import threading
 import urllib.request
 import queue
-
-
 from apa102 import APA102
 
-def get_url(account_key, log_set_name, log_name, start, end):
-    return "https://pull.logentries.com/{:s}/hosts/{:s}/{:s}/?start={:d}&end={:d}&filter=/router.*POST.*orders\"/".format(account_key, log_set_name, log_name, start, end)
+class LogEntries():
+    def __init__(self, account_key, log_set_name, log_name):
+        self.account_key = account_key
+        self.log_set_name = log_set_name
+        self.log_name = log_name
 
-def load_logs(account_key, log_set_name, log_name, start, end):
-    r=urllib.request.urlopen(get_url(account_key, log_set_name, log_name, start, end))
-    body=r.read().decode('utf-8')
-    events=[]
-    for line in body.split('\n'):
-        m = re.match(r'.*?(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{6}[-+]\d{2}:\d{2}).*path="([^"]+)".*', line)
-        if m:
-            when=datetime.datetime.strptime(m.groups()[0], '%Y-%m-%dT%H:%M:%S.%f+00:00')
-            events.append((when, m.groups()[1]))
+    def get_url(self, start, end):
+        log_filter='/router.*POST.*orders\".*status=200/'
+        return "https://pull.logentries.com/{:s}/hosts/{:s}/{:s}/?start={:d}&end={:d}&filter={:s}".format(self.account_key, self.log_set_name, self.log_name, start, end, log_filter)
 
-    return events
+    def load_logs(self, start, end):
+        r=urllib.request.urlopen(self.get_url(start, end))
+        body=r.read().decode('utf-8')
+        events=[]
+        for line in body.split('\n'):
+            m = re.match(r'.*?(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{6}[-+]\d{2}:\d{2}).*path="([^"]+)".*', line)
+            if m:
+                when=datetime.datetime.strptime(m.groups()[0], '%Y-%m-%dT%H:%M:%S.%f+00:00')
+                events.append((when, m.groups()[1]))
+
+        return events
 
 class LedPattern():
-    buffer=None
+    buff=None
     def __init__(self, leds, count, speed):
         self.leds = leds
-        self.buffer = collections.deque([0] * count, count)
+        self.buff = collections.deque([0] * count, count)
         self.speed = speed
         self.counter = 0
 
@@ -42,25 +47,25 @@ class LedPattern():
             self.advance(0)
 
     def advance(self, value):
-        self.buffer.appendleft(value)
+        self.buff.appendleft(value)
         self.show()
 
     def show(self):
-        for i, rgb in enumerate(self.buffer):
+        for i, rgb in enumerate(self.buff):
             self.leds.setPixelRGB(i, rgb)
         self.leds.show()
-
-config = None
-with open("config.json") as config_file:
-    config=json.load(config_file)
 
 events = queue.Queue()
 load_lock = threading.Event()
 
 def load_events():
     last_load=None
+    config = None
+    with open("config.json") as config_file:
+        config=json.load(config_file)
+    logentries = LogEntries(config['account_key'], config['log_set_name'], config['log_name'])
     while True:
-        load_lock.wait() 
+        load_lock.wait()
         print("Loading events...")
 
         interval=60 * 1000
@@ -73,7 +78,7 @@ def load_events():
             now=int(time.time() * 1000)
         else:
             last_load = now - interval
-        logs = load_logs(config['account_key'], config['log_set_name'], config['log_name'], last_load, now)
+        logs = logentries.load_logs(last_load, now)
         for event in sorted(logs, key=lambda event: event[0]):
             events.put(event)
         print("Loaded {:d} events.".format(len(logs)))
@@ -85,10 +90,11 @@ t.daemon=True
 t.start()
 
 # offset from realtime
-offset=datetime.timedelta(seconds=65)
+offset=datetime.timedelta(seconds=60 + 5)
 
-leds=LedPattern(APA102(32), 32, 32)
+led_count=32
 fs=44100
+leds=LedPattern(APA102(led_count), led_count, 32)
 
 event = None
 while True:
